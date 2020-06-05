de467d0c2060a54c4e2fa7eecd426803871c156a
seeoya <54855563+seeoya@users.noreply.github.com>
2020-06-05 15:01:31 +0900

    2020. 06. 05. 수정
    정렬 추가
    제목 변경

--- SpreadSheet/mySet.js
+++ SpreadSheet/mySet.js
@@ -1,22 +1,22 @@
-
 /*
-  최초 업로드 2020. 05. 19. / 최종 수정 2020. 05. 19.
+  2020. 06. 05.
   https://github.com/seeoya/lalllaby
-  구글 스프레드 시트 행 크기 설정하는 스크립트 백업
+  구글 스프레드 시트 해당 범위 정렬 + 행 크기 설정하는 스크립트 백업
 */
 
-function myRowHeightSet() {
+function mySet() {
   
   /* 행 크기 설정할 시트의 순서 입력
      좌측 시트부터 1, 2, 3 카운트 */
   var sheetNumber = 2;
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[sheetNumber-1];
   
   /* 행 크기 설정할 범위 지정 (startNum=시작행, endNum=종료행)
      스프레드 시트 기준 좌측 행 넘버 입력 */
-  var startNum = 247;
-  var endNum = 282;
+  var startNum = 230;
+  var endNum = 237;
   
-  /* 행 크기 설정의 기준이 되는 열의 넘버 (1, 2, 3...) */
+  /* 행 크기 설정 기준이 되는 열의 넘버 (A부터 1, 2, 3...) */
   var countColumn = 8;
   
   /* 기본 행 크기(1줄) */
@@ -24,13 +24,21 @@ function myRowHeightSet() {
   /* 엔터당 추가될 행 크기(2줄~) */
   var height2 = 15;
   
+  /* 정렬에 필요한 범위 */
+  var rangeString = "B" + startNum + ":K" + endNum; 
+  
+  /* 정렬 기준 열의 넘버 (A부터 1, 2, 3...) */
+  var sortByColumn = 4;
+  
   /////////////////////////////////////////
   
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[sheetNumber-1];
+  /* 해당 열 기준 정렬 */
+  var range = sheet.getRange(rangeString);
+  range.sort({column:sortByColumn});
   
+  /* 행 크기 설정 */
   for(i = startNum; i <= endNum; i++) {
     var any = (sheet.getRange(i, countColumn).getValue().toString().split("\n").length)-1;
     sheet.setRowHeight(i, height1+(any*height2));
   }
 }
- 

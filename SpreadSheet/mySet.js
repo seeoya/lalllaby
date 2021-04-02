a37005fc56d06a7e59b99a47e2d3d545cd0708c9
수영 <54855563+seeoya@users.noreply.github.com>
2021-04-02 11:48:22 +0900

    2021. 04. 02. 백업

--- SpreadSheet/mySet.js
+++ SpreadSheet/mySet.js
@@ -1,5 +1,5 @@
 /*
-  2020. 06. 05.
+  2021. 04. 02.
   https://github.com/seeoya/lalllaby
   구글 스프레드 시트 해당 범위 정렬 + 행 크기 설정하는 스크립트 백업
 */
@@ -8,13 +8,13 @@ function mySet() {
   
   /* 행 크기 설정할 시트의 순서 입력
      좌측 시트부터 1, 2, 3 카운트 */
-  var sheetNumber = 2;
+  var sheetNumber = 1;
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[sheetNumber-1];
   
   /* 행 크기 설정할 범위 지정 (startNum=시작행, endNum=종료행)
      스프레드 시트 기준 좌측 행 넘버 입력 */
-  var startNum = 230;
-  var endNum = 237;
+  var startNum = sheet.getRange(1, 3).getValue();
+  var endNum = sheet.getRange(1, 4).getValue();
   
   /* 행 크기 설정 기준이 되는 열의 넘버 (A부터 1, 2, 3...) */
   var countColumn = 8;
@@ -32,13 +32,18 @@ function mySet() {
   
   /////////////////////////////////////////
   
-  /* 해당 열 기준 정렬 */
-  var range = sheet.getRange(rangeString);
-  range.sort({column:sortByColumn});
-  
-  /* 행 크기 설정 */
-  for(i = startNum; i <= endNum; i++) {
-    var any = (sheet.getRange(i, countColumn).getValue().toString().split("\n").length)-1;
-    sheet.setRowHeight(i, height1+(any*height2));
-  }
+  if(sheet.getRange(1, 2).isChecked()) {
+
+    /* 해당 열 기준 정렬 */
+    var range = sheet.getRange(rangeString);
+    range.sort({column:sortByColumn});
+    
+    /* 행 크기 설정 */
+    for(i = startNum; i <= endNum; i++) {
+      var any = (sheet.getRange(i, countColumn).getValue().toString().split("\n").length)-1;
+      sheet.setRowHeight(i, height1+(any*height2));
+    }
+
+    sheet.getRange(1, 2).setValue('FALSE');
+  }  
 }

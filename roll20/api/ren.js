f73c078a1e6203024a3729158c2b176e44fec5f8
수영 <54855563+seeoya@users.noreply.github.com>
2021-06-23 16:21:28 +0900

    Update ren.js

--- roll20/api/ren.js
+++ roll20/api/ren.js
@@ -1,45 +1,70 @@
-
 // 최초 업로드 2021. 06. 23. / 최종 수정 2021. 06. 23.
 // https://github.com/seeoya/lalllaby
 // Roll20 attr 증가, 감소 api
 
-// ex. !ren 10 (+10), !ren -10 (-10), !ren d10(+1d10), !ren -d30(-1d30)
+// ex. !ren 10 (+10), !ren -10 (-10), !ren 1d10(+1d10), !ren -4d30(-4d30)
 
-on('chat:message', msg => {
-    if(msg.type === 'api') {
-        
+on("chat:message", msg => {
+    if (msg.type === "api") {
         // 사용할 attr 이름 입력. 예시는 ren.
-        let attrName = 'ren';
-        
+        let attrName = "ren";
+
         if (msg.content.indexOf("!ren ") === 0) {
             try {
-                let character = findObjs({type:'character', name: msg.who})[0],
+                let character = findObjs({ type: "character", name: msg.who })[0],
                     cont = msg.content.replace("!ren ", ""),
+                    mFlag = false,
                     dFlag = cont.indexOf("d"),
-                    num = 0;
-                    
-                    if(character) {
-                        if(dFlag > -1) {
-                            cont = cont.replace("d", "");
-                            num = Math.floor((Math.random() * (parseInt(cont) - 1)) + 1);
-                        } else {
-                            num = parseInt(cont);
-                        }
-                    
-                        if(num) {
-                            attr = findObjs({type:'attribute', characterid: character.id, name: attrName})[0],
-                            attrC = parseInt(attr.get('current')),
-                            attrN = attrC + num;
-                            attr.set('current', attrN);
-                        }
-                        
-                        // 채팅창 기록이 필요하다면 하단 주석(//)을 해제하고 사용.
-                        // sendChat(msg.who, attrC + " + " + num + " → " + attrN);
+                    sig = " + ",
+                    cont1 = 0,
+                    cont2 = 0,
+                    rand = 0,
+                    num = 0,
+                    attrText = "";
+
+                    if(cont.indexOf("-") > -1) {
+                        mFlag = true;
+                        sig = " - ";
+                        cont = cont.replace("-", "");
                     } else {
-                        sendChat("error", "/w " + msg.who + "형식오류. 다시 시도해주세요!")
+                        cont = cont.replace("+", "");
                     }
-            } catch(err) {
-                sendChat('error', '/w gm ' + err);
+
+                if (character) {
+                    if (dFlag > -1) {
+                        cont1 = parseInt(cont.split("d")[0]);
+                        cont2 = parseInt(cont.split("d")[1]);
+
+                        for (let idx = 0; idx < cont1; idx++) {
+                            rand = Math.floor(Math.random() * (cont2 - 1) + 1);
+                            num += rand;
+                            attrText += rand;
+                            if (idx != cont1 - 1) { attrText += ", "; }
+                        };
+
+                        attrText = " ( " + cont.toUpperCase() + " : " + attrText + " )";
+                        num = parseInt(num);
+                    } else {
+                        num = parseInt(cont);
+                    }
+                    
+                    attr = findObjs({ type: "attribute", characterid: character.id, name: attrName })[0];
+                    attrC = parseInt(attr.get("current"));
+                    if(mFlag) {
+                        attrN = attrC - num;
+                    } else {
+                        attrN = attrC + num;
+                    }
+
+                    attr.set("current", attrN);
+
+                    // 채팅창 기록이 필요하다면 하단 주석(//)을 해제하고 사용.
+                    // sendChat(msg.who, attrC + sig + num + attrText + " → " + attrN);
+                } else {
+                    sendChat("error", "/w " + msg.who + "형식오류. 다시 시도해주세요!");
+                }
+            } catch (err) {
+                sendChat("error", "/w gm " + err);
             }
         }
     }
